db8c2524eda5371227c130eb79eee875cca24d65
BROCODES2024 <chetanreddyk394@gmail.com>
2025-02-06 19:21:38 +0530

    File structure update

--- backend/db/expenseModel.js
+++ backend/db/expenseModel.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const expenseSchema = new mongoose.Schema({
-    usersid : {
-        type :  mongoose.Schema.Types.ObjectId,
-        required : true,
-        ref : 'users'
+const expenseSchema = new mongoose.Schema(
+  {
+    usersid: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "users",
     },
-    amount : {
-        type : Number,
-        required : true 
+    amount: {
+      type: Number,
+      required: true,
     },
-    category : {
-        type : String,
-        required : true,
+    category: {
+      type: String,
+      required: true,
     },
-    date : {
-        type: String,
-        required : true,
-    }
-},{
-    timestamps : true
-})
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const expenseModel = mongoose.model('expenses' , expenseSchema);
+const expenseModel = mongoose.model("expenses", expenseSchema);
 
 module.exports = expenseModel;
